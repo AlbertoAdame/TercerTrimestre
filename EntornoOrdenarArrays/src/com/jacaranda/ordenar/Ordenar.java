d89b93b9c57b4e4cc84759b6a394b09ae8529faa
AlbertoAdame <alberto.aladamero@gmail.com>
2022-04-03 17:03:22 +0200

    proyecto entorno

--- EntornoOrdenarArrays/src/com/jacaranda/ordenar/Ordenar.java
+++ EntornoOrdenarArrays/src/com/jacaranda/ordenar/Ordenar.java
@@ -7,7 +7,7 @@ public class Ordenar {
 		
 		int numeros[]= new int[10];
 		
-		numeros[0]=0;
+//		numeros[0]=0;
 		numeros[1]=5;
 		numeros[2]=4;
 		numeros[3]=2;
@@ -18,6 +18,13 @@ public class Ordenar {
 		numeros[8]=8;
 		numeros[9]=1;
 		
+//		Si ordenamos un array vacio pone todo ceros
+//		Con un solo elemtento no da error
+//		Con dos no da error
+//		Si queda uno sin poner no da error
+		
+		
+		
 		
 		System.out.println(ordenar(numeros));
 //		probamos con array vacio, un elemento, dos elementes, ordenar todos menos 1, otro ordenar todos, y otro que no me acuerdo
@@ -27,16 +34,16 @@ public class Ordenar {
 	public static String ordenar(int numeros[]) {
         int i, j, menor, pos, tmp;
         String resultado="";
-        for (i = 0; i < numeros.length - 1; i++) {      // tomamos como menor el primero
-              menor = numeros[i];                       // de los elementos que quedan por ordenar                    
-              pos = i;                            // y guardamos su posición
-              for (j = i + 1; j < numeros.length; j++){ // buscamos en el resto
-                    if (numeros[j] < menor) {           // del array algún elemento
-                        menor = numeros[j];             // menor que el actual
+        for (i = 0; i < numeros.length - 1; i++) {      
+              menor = numeros[i];                                       
+              pos = i;                        
+              for (j = i + 1; j < numeros.length; j++){
+                    if (numeros[j] < menor) {          
+                        menor = numeros[j];           
                         pos = j;
                     }
               }
-              if (pos != i){                      // si hay alguno menor se intercambia                         
+              if (pos != i){                                            
                   tmp = numeros[i];
                   numeros[i] = numeros[pos];
                   numeros[pos] = tmp;
